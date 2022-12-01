ec50519f6bb4d3344255ca36b51958ba52308703
Gilbmet <gilbmet254@gmail.com>
2022-12-01 12:59:08 -0800

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python3
-from sys import argv, exit
-from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
-    arglen = len(argv) - 1
-    if arglen == 0:
-        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
-        exit(1)
-    op = argv[2]
-    a, b = int(argv[1]), int(argv[3])
-    if op == "+":
-        print('{} + {} = {}'.format(a, b, add(a, b)))
-    elif op == "-":
-        print('{} - {} = {}'.format(a, b, sub(a, b)))
-    elif op == "*":
-        print('{} * {} = {}'.format(a, b, mul(a, b)))
-    elif op == "/":
-        print('{} / {} = {}'.format(a, b, div(a, b)))
-    else:
-        print('Unknown operator. Available operators: +, -, * and /')
-        exit(1)
+    """Handle basic arithmetic operations."""
+    from calculator_1 import add, sub, mul, div
+    import sys
+
+    if len(sys.argv) - 1 != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(ops.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
