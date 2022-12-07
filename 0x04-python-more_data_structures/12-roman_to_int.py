3a33c34c661d917350b4269adb82215be9c46227
Gilbmet <gilbmet254@gmail.com>
2022-12-07 13:38:43 -0800

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,12 +1,45 @@
 #!/usr/bin/python3
-
-
 def roman_to_int(roman_string):
-    if not roman_string or type(roman_string) != str:
+    # Fail checks, none, not a string
+    if not roman_string:
         return 0
-    total = 0
-    digits = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
-    for roman in reversed(roman_string):
-        arabic = digits[roman]
-        total += arabic if total < arabic * 5 else -
-    return total
+    if not isinstance(roman_string, str):
+        return 0
+    if not roman_string.isupper():
+        return 0
+    # Dictionary for roman numerals
+    r_dict = {
+        "I": 1,
+        "IV": 4,
+        "V": 5,
+        "IX": 9,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
+
+    result = 0
+    temp = list(roman_string)
+    # Concat 4 and 9s
+    if len(temp) > 1:
+        idx = 0
+        for i in temp:
+            try:
+                if temp[idx] == 'I' and temp[idx + 1] == 'V':
+                    temp[idx:idx + 2] = [''.join(temp[idx:idx + 2])]
+            except IndexError:
+                pass
+            try:
+                if temp[idx] == 'I' and temp[idx + 1] == 'X':
+                    temp[idx:idx + 2] = [''.join(temp[idx:idx + 2])]
+            except IndexError:
+                pass
+            idx += 1
+        # Search in dict for correct numbers and add
+        for k, v in r_dict.items():
+            for index in temp:
+                if index == k:
+                    result += v
+        return result
