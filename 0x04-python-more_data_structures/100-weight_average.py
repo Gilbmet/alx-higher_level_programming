7c17a0bbd8f4fe10f55f51af6638cfb2f471ef7e
Gilbmet <gilbmet254@gmail.com>
2022-12-07 03:22:50 -0800

    100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
-def square_matrix_map(matrix=[]):
-    return list(map((lambda row: list(map((lambda x: x * x), row))), matrix))
+def weight_average(my_list=[]):
+    if my_list and len(my_list):
+        num = 0
+        denom = 0
+        for tup in my_list:
+            num += (tup[0] * tup[1])
+            denom += (tup[1])
+        return (num/denom)
+    return 0
