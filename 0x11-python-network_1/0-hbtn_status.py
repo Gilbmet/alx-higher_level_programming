f593e185886e9ed9ed8bf84c4816216c08159455
Gilbmet <gilbmet254@gmail.com>
2023-03-31 15:42:48 -0700

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3
-"""A script that
-- fetches https://alx-intranet.hbtn.io/status.
-- uses urlib package
-"""
+import urllib.request
 
-
-if __name__ == '__main__':
-    import urllib.request
-
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
-        content = res.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+url = 'https://alx-intranet.hbtn.io/status'
+with urllib.request.urlopen(url) as response:
+    body = response.read()
+    print('Body response:')
+    print('\t- type: {}'.format(type(body)))
+    print('\t- content: {}'.format(body))
+    print('\t- utf8 content: {}'.format(body.decode('utf-8')))
         
