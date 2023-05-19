078ff28f65f5b7843368a049b7832c6891d391c9
Gilbmet <gilbmet254@gmail.com>
2023-05-19 02:52:08 -0700

    3-error_code.py

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module to send a request to a URL and display the body of the response
+handling HTTP errors.
+"""
+
+import urllib.request
+import urllib.error
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    try:
+        with urllib.request.urlopen(url) as response:
+            body = response.read().decode('utf-8')
+            print(body)
+    except urllib.error.HTTPError as e:
+        print("Error code:", e.code)
