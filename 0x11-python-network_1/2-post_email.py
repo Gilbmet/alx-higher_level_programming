77e23f867c14f05536f186693b1d184dd7266b3c
Gilbmet <gilbmet254@gmail.com>
2023-05-19 02:28:54 -0700

    2-post_email.py

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Module to send a POST request to a URL with an email parameter and display the body of the response.
+"""
+
+import urllib.request
+import urllib.parse
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+    req = urllib.request.Request(url, data=data)
+
+    with urllib.request.urlopen(req) as response:
+        body = response.read().decode('utf-8')
+
+    print(body)
